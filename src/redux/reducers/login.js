11fd9053bfb32eb57120e4acb8fc6abd876efb43
Carlos Leal <carlos.frleal@gmail.com>
2023-05-23 00:50:53 -0300

    refatorado Login.js para remover o Warning do console

--- src/redux/reducers/login.js
+++ src/redux/reducers/login.js
@@ -1,4 +1,4 @@
-import { ADD_USER, GET_TOKEN } from '../actions/loginAction';
+import { ADD_USER } from '../actions/loginAction';
 
 const INITIAL_STATE = {
   user: {
@@ -12,9 +12,9 @@ const loginReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case ADD_USER:
     return { ...state, user: action.payload };
-  case GET_TOKEN:
-    localStorage.setItem('token', action.payload.token);
-    return { ...state, token: action.payload };
+  // case GET_TOKEN:
+  //   localStorage.setItem('token', action.payload.token);
+    // return { ...state, token: action.payload };
   default:
     return state;
   }
